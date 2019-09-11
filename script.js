d917a1841c55c696483648791d395894ff51af7e
ulucay <31716850+ulucay@users.noreply.github.com>
2019-09-10 22:42:03 -0500

    initial

---
+++ script.js
@@ -0,0 +1,32 @@
+let colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+];
+
+let squares = document.querySelectorAll(".square");
+let pickedColor = colors[3];
+let colorDisplay = document.querySelector("#colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(let i = 0 ; i < squares.length; i++){
+    //add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", () => {
+        let clickedColor = this.style.backgroundColor;
+
+        if(clickedColor === pickedColor){
+            alert("Correct!");
+        }
+        else{
+            alert("Wrong!");
+        }
+    });
+}
+
